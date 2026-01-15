0da7f64993db14a9eec13b072cb0fb110c9e15de
Fialco <113672867+Fialco@users.noreply.github.com>
2026-01-15 19:07:38 +0200

    Add user for testing

--- phoneypay/models.py
+++ phoneypay/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.TextField()
+    balance = models.IntegerField()
