74b7157b9a7cee27cc4b4b6a19b526cfbf0eca76
Fialco <113672867+Fialco@users.noreply.github.com>
2026-01-15 20:02:53 +0200

    Add send money form

--- phoneypay/views.py
+++ phoneypay/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .models import Account
 
 
 @login_required
 def index(request):
     account = Account.objects.get(owner__username=request.user)
-    return render(request, "pages/index.html", {"account": account})
+    numbers = Account.objects.filter(~Q(phone=account.phone))
+    return render(request, "pages/index.html", {"account": account, "numbers": numbers})
